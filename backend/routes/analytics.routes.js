1fcd2447f77f27b58fe7591ca5135d98c01415cc
Jovanny De Leon <roanlandao05@gmail.com>
2026-03-31 17:32:12 +0800

    delete

--- backend/routes/analytics.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getTodayAnalytics, getHistory } = require("../controllers/analytics.controller");
-const { protect } = require("../middleware/auth.middleware");
-
-router.get("/today", protect, getTodayAnalytics);
-router.get("/history", protect, getHistory);
-
-module.exports = router;
